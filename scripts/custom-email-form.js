860020a55c2f6262038c19eaf9186aeaaaeb4565
John Mangel <jmangel@localhost.localdomain>
2017-02-05 13:43:29 -0500

    Add email form to homepage banner; add transparent white background to banner text

---
+++ scripts/custom-email-form.js
@@ -0,0 +1,10 @@
+var customEmailFormButton = document.querySelector("#custom-email-form-button");
+if (customEmailFormButton){
+	customEmailFormButton.addEventListener("click",function(){
+		var emailForm = document.querySelector("#block-yui_3_17_2_2_1486256658221_46104 > div > div > span");
+		if (emailForm) emailForm.click();
+		else console.log("emailForm doesn't exist");
+	});
+} else {
+	console.log("customEmailFormButton doesn't exist");
+}
