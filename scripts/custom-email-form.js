8f5c3e0df09a42f0bbbdd9bf705936f8543b0790
John Mangel <jmangel@localhost.localdomain>
2017-02-07 01:42:25 -0500

    Section h1 tags and track form submission with facebook pixel

--- scripts/custom-email-form.js
+++ scripts/custom-email-form.js
@@ -6,3 +6,33 @@ if (customEmailFormButton){
 		// else console.log("emailForm doesn't exist");
 	});
 } // else console.log("customEmailFormButton doesn't exist");
+
+var eventposted=0;  
+var interval;
+
+$(window).load(function(){
+	var button = $(".homepage .lightbox-inner .button");
+	if (button) button.click(function() {
+		interval = window.setInterval(handleFormSubmission, 100);
+	});
+});
+function handleFormSubmission(){
+	// Track facebook lead and hide form title if submission successful
+	if(($(".form-submission-text").is(':visible')) && (eventposted==0)){
+		eventposted = 1;
+		if (interval) clearInterval(interval);
+		$(".homepage .lightbox-inner .form-title").hide();
+		fbq('track', 'Lead');
+	}
+	var errors = $('.field-error');
+	// Stop checking if form returns error
+	if(errors.length > 0){
+		if (interval) clearInterval(interval);
+	}
+	// Show generic error if no specific errors given
+	if(errors.length == 1){
+		var csstxt = "display: block !important;";
+		// csstxt = $('form>.field-error').css('cssText') + "display: block !important;"
+		$('form>.field-error').css("cssText", csstxt);
+	}
+}
