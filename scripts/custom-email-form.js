44527dd2263df620026472b186ae189d6b4887b1
John Mangel <jmangel@localhost.localdomain>
2017-03-22 14:14:38 -0400

    Add functionality similar to PMF page (ie. buttons, forms) to Investors page

--- scripts/custom-email-form.js
+++ scripts/custom-email-form.js
@@ -1,33 +1,70 @@
 var form;
 
-// Setup homepage banner mock email form button to trigger true email form
-var customEmailFormButton = document.querySelector("#custom-email-form-button");
-if (customEmailFormButton){
-	customEmailFormButton.addEventListener("click",function(){
-		var emailForm = $("#block-yui_3_17_2_2_1486256658221_46104 > div > div > span");
-		if (emailForm.length > 0) {
-			emailForm.click();
-		} else {
-			var emailForm2 = $("#block-yui_3_17_2_3_1489523619726_9461 > div > div > span");
-			emailForm2.click();
-		}
-		// else console.log("emailForm doesn't exist");
-	});
-} // else console.log("customEmailFormButton doesn't exist");
+/*Setup mock email form buttons to trigger true email forms*/
 
-// Remove redirect and add modal form functionality to PMF page sign up buttons
-var signUpButtons = $(".sqs-block-button-element");
-signUpButtons.removeAttr("href");
-signUpButtons.click(function(){
+/* Homepage */
+var customEmailFormButtonHomepage = 
+  document.querySelector("#collection-589622f4d1758ebabec92264 #custom-email-form-button");
+if (customEmailFormButtonHomepage){
+	customEmailFormButtonHomepage.addEventListener("click",function(){
+		var emailFormHomepage = $("#block-yui_3_17_2_2_1486256658221_46104 > div > div > span");
+		if (emailFormHomepage.length > 0){
+			emailFormHomepage.click();
+		}
+	});
+} 
+/* PMF page */
+var customEmailFormButtonPMF = 
+  document.querySelector("#collection-58c5b29d893fc021adf872bc #custom-email-form-button");
+if (customEmailFormButtonPMF){
+	customEmailFormButtonPMF.addEventListener("click",function(){
+		var emailFormPMF = $("#block-yui_3_17_2_3_1489523619726_9461 > div > div > span");
+		if (emailFormPMF.length > 0){
+			emailFormPMF.click();
+		}
+	});
+} 
+/* Investors page */
+var customEmailFormButtonInvestors = 
+  document.querySelector("#collection-58d2b4a4a5790a8c5c0c0c4c #custom-email-form-button");
+if (customEmailFormButtonInvestors){
+	customEmailFormButtonInvestors.addEventListener("click",function(){
+		var emailFormInvestors = $("#block-39358caf2ef767205d99 > div > div > span");
+		if (emailFormInvestors.length > 0){
+			emailFormInvestors.click();
+		}
+	});
+} 
+
+
+/* Remove redirect and add modal form functionality to PMF page sign up buttons */
+var pmfSignUpButtons = $("#collection-58c5b29d893fc021adf872bc .sqs-block-button-element");
+pmfSignUpButtons.removeAttr("href");
+pmfSignUpButtons.click(function(){
 	var emailForm = $("#block-yui_3_17_2_3_1489523619726_9461 > div > div > span");
 	emailForm.click();
 });
 
-//add "We respect your privacy" to email form on PMF page
+/* Remove redirect and add modal form functionality to Investors page sign up buttons */
+var investorSignUpButtons = $("#collection-58d2b4a4a5790a8c5c0c0c4c .sqs-block-button-element");
+investorSignUpButtons.removeAttr("href");
+investorSignUpButtons.click(function(){
+	var emailForm = $("#block-39358caf2ef767205d99 > div > div > span");
+	emailForm.click();
+});
+
+/* Add "We respect your privacy" to email form on PMF page */
 var privacyDisclaimerHtmlString = "<span style='display: block; font-family: \"futura-pt\"; font-size: 12px; margin-top: 30px;'>We respect your <a href='/privacy-policy' style='text-decoration: underline;'>privacy</a></span>";
+/* PMF page */
 $("#collection-58c5b29d893fc021adf872bc .form-button-wrapper").append(privacyDisclaimerHtmlString);
+/* Investors page */
+$("#collection-58d2b4a4a5790a8c5c0c0c4c .form-button-wrapper").append(privacyDisclaimerHtmlString);
 // $("#collection-58c5b29d893fc021adf872bc .lightbox-content").css('padding-bottom', '10px');
 
+
+
+
+/* Track form submissions */
 
 // Set new submit handler on form each time the form is opened
 // because SquareSpace resets form elements and ids when opened/closed
